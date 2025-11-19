d344f4f69144d7a20822b6b49d9e0772a96c4599
helenewegener1 <helene.wegener@gmaill.com>
2025-11-19 16:23:21 +0100

    don't load in ADT in the beginning

--- 06_seurat_load/script/load_seurat.R
+++ 06_seurat_load/script/load_seurat.R
@@ -5,7 +5,7 @@ library(Seurat)
 library(glue)
 
 # Samples
-samples <- list.files("05_run_cellranger/out/") %>% str_split_i("_", 2)
+samples <- list.files("05_run_cellranger/out_main/") %>% str_split_i("_", 2)
 
 # Prep out list
 seurat_obj_list <- list()
@@ -18,7 +18,7 @@ for (sample in samples){
   # Define path to cellranger output
   # The files in the per_sample_outs directory have been demultiplexed to single samples.
   # Read more about output of cellrange multi: https://www.10xgenomics.com/support/software/cell-ranger/latest/analysis/outputs/cr-3p-outputs-cellplex
-  OUTS_DIR <- glue("05_run_cellranger/out/res_{sample}/outs/per_sample_outs/res_{sample}")
+  OUTS_DIR <- glue("05_run_cellranger/out_main/res_{sample}/outs/per_sample_outs/res_{sample}")
   
   # Read GEX counts (the default method for 10x data)
   gex.data <- Read10X(data.dir = glue("{OUTS_DIR}/count/sample_filtered_feature_bc_matrix"))
@@ -51,14 +51,6 @@ for (sample in samples){
     # Add ADT data as a new assay (e.g., "ADT")
     seurat_obj[["ADT"]] <- CreateAssayObject(counts = adt_counts_aligned) 
     
-    # Normalize the ADT data (often using the CLR method)
-    seurat_obj <- NormalizeData(seurat_obj, assay = "ADT", normalization.method = "CLR")
-
-    # Find variable features in the ADT assay (optional but good practice)
-    seurat_obj <- HTODemux(seurat_obj, assay = "ADT", positive.quantile = 0.999)
-    seurat_obj@meta.data$ADT_classification.global %>% table()
-    seurat_obj@meta.data$ADT_maxID %>% table()
-    
     ################### If only gene expression data is available ################### 
   } else { 
     
@@ -74,82 +66,82 @@ for (sample in samples){
   
   #################### Import VDJ Annotation Files (TCR and BCR) ##################### 
 
-  tcr_meta <- NULL
-  bcr_meta <- NULL
-  
-  # Check if BCR data is available 
-  if ("vdj_t" %in% list.files(OUTS_DIR)){
-    tcr_meta <- read.csv(glue("{OUTS_DIR}/vdj_t/filtered_contig_annotations.csv"))
-  } 
-  
-  if ("vdj_b" %in% list.files(OUTS_DIR)){
-    bcr_meta <- read.csv(glue("{OUTS_DIR}/vdj_b/filtered_contig_annotations.csv"))
-  } 
-
-  # Wrangling
-  
-  # Can we safely remove barcode from contig ids
-  # table(tcr_meta$barcode == str_split_i(tcr_meta$contig_id, "_", 1))
-  # table(bcr_meta$barcode == str_split_i(bcr_meta$contig_id, "_", 1))
-
-  # Wrangle TCR data so it can be added to the metadata of the seurat object
-  if (!is.null(tcr_meta)){
-    
-    index_barcode <- which(colnames(tcr_meta) == "barcode")
-    index_contig_id <- which(colnames(tcr_meta) == "contig_id")
-    
-    tcr_meta_clean <- tcr_meta %>% 
-      mutate(contig_id = str_remove_all(contig_id, glue("{barcode}_"))) %>% 
-      pivot_wider(id_cols = barcode,
-                  names_from = contig_id, 
-                  values_from = colnames(tcr_meta)[-c(index_barcode, index_contig_id)]) %>% 
-      column_to_rownames("barcode")
-    
-    # Mutate all "" to NA
-    tcr_meta_clean[tcr_meta_clean == ""] <- NA
-    
-    # # Filter on UMI if 2*TRB?
-    # tcr_meta_clean %>% select(chain_contig_1, chain_contig_2, umis_contig_1, umis_contig_2) %>% 
-    #   mutate(same = ifelse(chain_contig_1 == chain_contig_2, TRUE, FALSE)) %>% 
-    #   filter(same == TRUE)
-    
-    # Add prefix to column names as bcr data has the same colnames
-    colnames(tcr_meta_clean) <- paste0("tcr_", colnames(tcr_meta_clean))
-    
-    # Add TCR data to seurat object
-    seurat_obj <- AddMetaData(
-      seurat_obj,
-      metadata = tcr_meta_clean
-    )
-    
-  }
-  
-  # Wrangle BCR data so it can be added to the metadata of the seurat object
-  if (!is.null(bcr_meta)){
-    
-    index_barcode <- which(colnames(bcr_meta) == "barcode")
-    index_contig_id <- which(colnames(bcr_meta) == "contig_id")
-    
-    bcr_meta_clean <- bcr_meta %>% 
-      mutate(contig_id = str_remove_all(contig_id, glue("{barcode}_"))) %>% 
-      pivot_wider(id_cols = barcode,
-                  names_from = contig_id, 
-                  values_from = colnames(bcr_meta)[-c(index_barcode, index_contig_id)]) %>% 
-      column_to_rownames("barcode")
-    
-    # Mutate all "" to NA
-    bcr_meta_clean[bcr_meta_clean == ""] <- NA
-    
-    # Add prefix to column names as tcr data has the same colnames
-    colnames(bcr_meta_clean) <- paste0("bcr_", colnames(bcr_meta_clean))
-    
-    # Add BCR data to seurat object
-    seurat_obj <- AddMetaData(
-      seurat_obj,
-      metadata = bcr_meta_clean
-    )
-    
-  }
+  # tcr_meta <- NULL
+  # bcr_meta <- NULL
+  # 
+  # # Check if BCR data is available 
+  # if ("vdj_t" %in% list.files(OUTS_DIR)){
+  #   tcr_meta <- read.csv(glue("{OUTS_DIR}/vdj_t/filtered_contig_annotations.csv"))
+  # } 
+  # 
+  # if ("vdj_b" %in% list.files(OUTS_DIR)){
+  #   bcr_meta <- read.csv(glue("{OUTS_DIR}/vdj_b/filtered_contig_annotations.csv"))
+  # } 
+  # 
+  # # Wrangling
+  # 
+  # # Can we safely remove barcode from contig ids
+  # # table(tcr_meta$barcode == str_split_i(tcr_meta$contig_id, "_", 1))
+  # # table(bcr_meta$barcode == str_split_i(bcr_meta$contig_id, "_", 1))
+  # 
+  # # Wrangle TCR data so it can be added to the metadata of the seurat object
+  # if (!is.null(tcr_meta)){
+  #   
+  #   index_barcode <- which(colnames(tcr_meta) == "barcode")
+  #   index_contig_id <- which(colnames(tcr_meta) == "contig_id")
+  #   
+  #   tcr_meta_clean <- tcr_meta %>% 
+  #     mutate(contig_id = str_remove_all(contig_id, glue("{barcode}_"))) %>% 
+  #     pivot_wider(id_cols = barcode,
+  #                 names_from = contig_id, 
+  #                 values_from = colnames(tcr_meta)[-c(index_barcode, index_contig_id)]) %>% 
+  #     column_to_rownames("barcode")
+  #   
+  #   # Mutate all "" to NA
+  #   tcr_meta_clean[tcr_meta_clean == ""] <- NA
+  #   
+  #   # # Filter on UMI if 2*TRB?
+  #   # tcr_meta_clean %>% select(chain_contig_1, chain_contig_2, umis_contig_1, umis_contig_2) %>% 
+  #   #   mutate(same = ifelse(chain_contig_1 == chain_contig_2, TRUE, FALSE)) %>% 
+  #   #   filter(same == TRUE)
+  #   
+  #   # Add prefix to column names as bcr data has the same colnames
+  #   colnames(tcr_meta_clean) <- paste0("tcr_", colnames(tcr_meta_clean))
+  #   
+  #   # Add TCR data to seurat object
+  #   seurat_obj <- AddMetaData(
+  #     seurat_obj,
+  #     metadata = tcr_meta_clean
+  #   )
+  #   
+  # }
+  # 
+  # # Wrangle BCR data so it can be added to the metadata of the seurat object
+  # if (!is.null(bcr_meta)){
+  #   
+  #   index_barcode <- which(colnames(bcr_meta) == "barcode")
+  #   index_contig_id <- which(colnames(bcr_meta) == "contig_id")
+  #   
+  #   bcr_meta_clean <- bcr_meta %>% 
+  #     mutate(contig_id = str_remove_all(contig_id, glue("{barcode}_"))) %>% 
+  #     pivot_wider(id_cols = barcode,
+  #                 names_from = contig_id, 
+  #                 values_from = colnames(bcr_meta)[-c(index_barcode, index_contig_id)]) %>% 
+  #     column_to_rownames("barcode")
+  #   
+  #   # Mutate all "" to NA
+  #   bcr_meta_clean[bcr_meta_clean == ""] <- NA
+  #   
+  #   # Add prefix to column names as tcr data has the same colnames
+  #   colnames(bcr_meta_clean) <- paste0("bcr_", colnames(bcr_meta_clean))
+  #   
+  #   # Add BCR data to seurat object
+  #   seurat_obj <- AddMetaData(
+  #     seurat_obj,
+  #     metadata = bcr_meta_clean
+  #   )
+  #   
+  # }
   
 
   # CRITICAL STEP: Add clonotype info (the actual T/B cell ID)
